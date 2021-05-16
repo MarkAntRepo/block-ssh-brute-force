ea705b168445f011a5c5def31dfd2e98eed88020
Antonio Markovic <s114119@ap.be>
2021-05-16 07:38:01 -0400

    Corrected the misunderstanding of the exercise

--- block_ssh_attempts.py
+++ block_ssh_attempts.py
@@ -6,39 +6,59 @@ import fwblock
 
 script_args = sys.argv[1:]
 option_string = 'hvn:v'
-opts, extra_args = getopt.getopt(script_args, option_string)
+try:
+    opts, extra_args = getopt.getopt(script_args, option_string)
+except getopt.GetoptError as e:
+    sys.stderr.write(" doesn't exist for more information use option -h")
+    sys.exit(1)
 
-for option, option_arg in opts:
-    if option == '-h':
-        print('Your options are -h(for help)', '\n' ,'-v(shows ips that would be blocked but a file needs to be loaded with -n "file name")','\n', "-n(reads file but doesn't call the block_ip function can be combined with -v to show what ips would be blocked)")
+
+def get_ips(LogFile,options):
+    try:
+        log_file = open(LogFile,'r')
+    except FileNotFoundErrot as err:
+        sys.stderr.write(str(err))
+
+    ips = []
+    ItemsInLine = []
+    IpsToBlock = []
+    for line in log_file.readlines():
+        if line.__contains__('Invalid user'):
+            ItemsInLine = line.split(" ")
+            ip = ItemsInLine[-3]
+            ips.append(ip)
+    IPList = set(ips)
+
+    for i in IPList:
+        amount = 0
+        for j in ips:
+            if i == j:
+                amount = amount + 1
+        if amount >= 3:
+            IpsToBlock.append(i)
+    return IpsToBlock
+
+if opts == [] and extra_args != []:
+    answer = get_ips(sys.argv[1],opts)
+    for i in answer:
+        fwblock.block_ip(i)
+
+if len(opts) == 1:
+    if '-h' in opts[0]:
+        print('Your options are -h(for help)', '\n' ,'-v(shows ips that are getting blocked but a file needs to be loaded with -v "file name")','\n', "-n 'file name' doesn't block and doesn't show what would be blocked by adding de option -v  example -n 'file name' -v then it show what would be block but doesn't block it.)")
         sys.exit(1)
-    elif option == '-v':
-        ips = []
-        ItemsInLine = []
-        for line in log_file.readlines():
-            if line.__contains__('Invalid user'):
-                ItemsInLine = line.split(" ")
-                ip = ItemsInLine[-3]
-                ips.append(ip)
-         
-        IPList = set(ips)
-         
-        for i in IPList:
-            aantal = 0
-            for j in ips:
-                if i == j: 
-                    aantal = aantal + 1
-            if aantal >= 3:
-                fwblock.block_ip(i)
-                print('Blocking',i,aantal)
-                print('-' * 70)
 
+    if '-v' in opts[0]:
+        answer = get_ips(sys.argv[2],opts)
+        for i in answer:
+            fwblock.block_ip(i)
+            print('blocking',i)
 
-    elif option == '-n':
-        try:
-            log_file = open(str(option_arg),'r')
-        except FileNotFoundError as err:
-            sys.stderr(str(err))
-    else :
-        sys.stderr(option, "doesn't exist for more information use option -h")
-        sys.exit(1)
+    if '-n' in opts[0]:
+        answer = get_ips(sys.argv[2],opts)
+
+elif len(opts) == 2:
+    if '-n' in opts[0] and '-v'in opts[1]:
+        answer = get_ips(sys.argv[2],opts)
+        for i in answer:
+            print('blocking',i)
